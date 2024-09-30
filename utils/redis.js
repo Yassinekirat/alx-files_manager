ea9d105b68a58a1765b9ae49e67b0646668e0bcf
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-30 17:07:30 +0100

    running eslint command

--- utils/redis.js
+++ utils/redis.js
@@ -4,27 +4,19 @@ class RedisClient {
   constructor() {
     this.client = createClient();
 
-    /** Handle Redis connection errors */
     this.client.on('error', (err) => {
       console.error(`Redis client not connected to the server: ${err.message}`);
     });
 
-    /** Connect to the Redis server */
     this.client.connect().catch((err) => {
       console.error(`Error connecting to Redis: ${err.message}`);
     });
   }
 
-  /** Check if Redis client is connected and alive */
   isAlive() {
     return this.client.isReady;
   }
 
-  /** 
-   * Get the value associated with the key
-   * @param {string} key - The key to retrieve the value for 
-   * @returns {Promise<string|null>} - The value of the key, or null if not found 
-   */
   async get(key) {
     try {
       const value = await this.client.get(key);
@@ -35,12 +27,6 @@ class RedisClient {
     }
   }
 
-  /** 
-   * Set a key-value pair with expiration
-   * @param {string} key - The key to set
-   * @param {string} value - The value to associate with the key
-   * @param {number} duration - The expiration time in seconds
-   */
   async set(key, value, duration) {
     try {
       await this.client.setEx(key, duration, value);
@@ -49,10 +35,6 @@ class RedisClient {
     }
   }
 
-  /** 
-   * Delete a key from Redis
-   * @param {string} key - The key to delete
-   */
   async del(key) {
     try {
       await this.client.del(key);
@@ -62,6 +44,5 @@ class RedisClient {
   }
 }
 
-/** Export an instance of RedisClient */
 const redisClient = new RedisClient();
 export default redisClient;
