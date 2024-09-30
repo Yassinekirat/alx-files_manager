57ab7a846d477cfda149246f14513dccc1aa2898
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-09-30 18:21:25 +0100

    eslint fix

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    (async () => {
+      try {
+        const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } catch (error) {
+        console.log(error.message);
+        this.db = null;
+      }
+    })();
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
