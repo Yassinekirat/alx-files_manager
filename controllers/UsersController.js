576a67aeb0c5f31fdfedf9c1175ed5464382c749
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-10-01 13:22:08 +0100

    lets see

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,41 @@
-#!/usr/bin/node
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const userExists = await dbClient.usersCollection.findOne({ email });
+      if (userExists) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const sha1Password = sha1(password);
+
+      const result = await dbClient.usersCollection.insertOne({
+        email,
+        password: sha1Password,
+      });
+
+      return res.status(201).json({
+        id: result.insertedId.toString(),
+        email: email,
+      });
+
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default UsersController;
